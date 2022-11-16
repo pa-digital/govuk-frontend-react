939f78986b92068d9537c8a84b32d214e8253791
PAAndrewMarshall <andrew.marshall@paconsulting.com>
2022-11-16 11:11:34 +0000

    Upgraded to GDS 4.4.0 Upgraded Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
+  managerWebpack: (config, options) => {
+      options.cache.set = () => Promise.resolve();
+      return config;
+  },
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/addon-postcss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
